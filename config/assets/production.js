d9c7fdb9fd584e1efe3e46623ffd358bae0fa236
DLiszewski <braciak2@gmail.com>
2015-12-10 20:21:12 +0100

    fixes

--- config/assets/production.js
+++ config/assets/production.js
@@ -6,7 +6,7 @@ module.exports = {
       css: [
         'public/lib/bootstrap/dist/css/bootstrap.min.css',
         'public/lib/bootstrap/dist/css/darkly.min.css',
-        'public/lib/bootstrap/dist/css/bootstrap-theme.min.css',
+        //'public/lib/bootstrap/dist/css/bootstrap-theme.min.css',
         'public/lib/angular-chart.js/dist/angular-chart.min.css'
       ],
       js: [
